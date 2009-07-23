566fb89213dffc1345a944b274c3c5cf83f9ce24
AlecSchueler <johannalecschueler@googlemail.com>
2009-07-23 16:51:49 +0100

    First draft of setup.py 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="delectus",
+    version="0.1α",
+    author="John Hill",
+    author_email="johannalecschueler@googlemail.com",
+    url="http://github.com/AlecSchueler/delectus",
+    license="LGPL v2",
+    data_files=[('man/man1',['man/delectus.1'])],
+    scripts=["delectus"],
+    classifiers=[
+        'Development Status :: 3 -Alpha',
+        'Enviroment :: Console',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        ]
+)
