12f6755a90769f58c676550196b4f50fddd3393e
AlecSchueler <johannalecschueler@googlemail.com>
2009-07-29 22:38:58 +0100

    time funcs in del_api

--- del_api.py
+++ del_api.py
@@ -1,4 +1,5 @@
 #!/usr/bi/env python import urllib import re
+from time import mktime
 import urllib
 import re
 
@@ -9,14 +10,22 @@ def sgroup(regex,string):
     return regex.search(string).group()
 
 def parse_attr(attr_name,string):
-    print string
     start= len(attr_name)+2 # id est 'attr_nam="'
     stop = -1
     
     return string[start:stop]
 
 def api_time_to_epoch(time):
-    
+    global months
+    #2009-06-19T17:28:55Z
+    parsed = (time[0:4],
+              time[5:6],
+              time[8:10],
+              time[-9:-7],
+              time[-6:-4],
+              time[-3:-1],
+              0,0,-1)
+    return int(mktime(tuple(int(i) for i in parsed)))
 
 def parse_posts(xml):
     post  = re.compile("<post .*?>\\n")
@@ -27,13 +36,15 @@ def parse_posts(xml):
     time  = re.compile("time=\".*?\"")
     
     parsed = {}
-    
+
+    #todo: order
     for p in post.findall(xml):
         parsed[parse_attr("description",sgroup(title,p))]=(
             parse_attr("href", sgroup( href,p)),
-            parse_attr("tag", sgroup( tags,p)),
+            tuple(parse_attr("tag", sgroup( tags,p)).split(' ')),
             parse_attr("extended", sgroup( desc,p)),
-            parse_attr("time", sgroup( time,p))
+            api_time_to_epoch(parse_attr("time", sgroup( time,p)))
         )
     return parsed
+
 
